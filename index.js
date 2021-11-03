03c1e53afdfcd6f72e8db26f44a9b264f088ad61
Your Name <mari@Maribels-MBP.hsd1.tx.comcast.net>
2021-11-03 15:48:27 -0500

    solved task 7

--- index.js
+++ index.js
@@ -45,9 +45,10 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  
 }    
+
 
 
 
@@ -160,9 +161,17 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, string){
+  const filtered = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].includes(string)){
+      filtered.push(array[i]);
+    }
+  }
+  return filtered;
 }
+console.log('task 7:', filterByWord(originalFlavors, 'Chocolate'));
+
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
